9cc4ca87e1d1fc222798a7742c77eeaf63fdd230
henriqvedev <dtrr2k21@gmail.com>
2025-04-19 09:09:10 -0300

    nova branch

---
+++ src/main/java/com/cadninjas/cadstroninjas/CadstroninjasApplication.java
@@ -0,0 +1,13 @@
+package com.cadninjas.cadstroninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadstroninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadstroninjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadninjas/cadstroninjas/CadstroninjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadninjas.cadstroninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadstroninjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
