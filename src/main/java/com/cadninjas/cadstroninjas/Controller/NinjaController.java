1cf908d8c6117006991736c957b55d0c121e3fbf
henriqvedev <dtrr2k21@gmail.com>
2025-04-21 10:45:28 -0300

    up

---
+++ src/main/java/com/cadninjas/cadstroninjas/Controller/NinjaController.java
@@ -0,0 +1,17 @@
+package com.cadninjas.cadstroninjas.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
